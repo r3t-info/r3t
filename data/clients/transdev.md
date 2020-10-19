c771dd563dac9d6f7b448f553075a2b565d84da5
bbkfed <balthazarberling+fed@gmail.com>
2020-10-19 21:29:02 +0200

    took away logo and replaced it with r3t h1, minor change on styling, copied the last text intro, our method and Adam from Transdev

--- data/clients/transdev.md
+++ data/clients/transdev.md
@@ -1,8 +1,8 @@
 ---
 order: 2
 name: Adam Fall
-position: Chief Information Officer & Chief Arhitect
+position: Chief Information Officer
 company: Transdev
 img: images/clients/temp-client.jpg
-content: R3T replaced our old integration hub and closed two other obsolete systems. In parallel, they have aligned our digital strategy by connecting systems for data transfer for our new data warehouse, building a reliable service enablement platform, with IoT capabilities for smart mobility services for our customers and the society.
+content: R3T replaced our old integration hub. In parallel, they have aligned our digital strategy based on rapid prototyping and reusable integrations. By this approach we get a reliable and sustainable service enablement platform with IoT capabilities, generating smart mobility services for our customers and the society.
 ---
--- pages/index.js
+++ pages/index.js
@@ -16,20 +16,19 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container p-2 space-y-10">
-        <header className="h-20 mb-16">
-          <img src="images/r3t-logo.svg" alt={`${name} Logo`} className="h-full m-2" />
+        <header className="mt-2">
+          <h1>{name}</h1>
         </header>
 
         <main className="space-y-16">
           <section id="who-we-are">
-            <h1 className="text-6xl font-bold tracking-tighter">{name}</h1>
             <h4>
-              We are a digital service company changing the way digital strategies, management and consulting are
-              delivered. We work with the 3T method based on Swedish research.*
+              We are a digital services company changing the way digital strategies, management, and consulting are
+              delivered. We work with the 3T method which is based on Swedish research.*
             </h4>
             <p>
-              *A 9-years collaboration between 40+ industry companies, public organisations and researchers from
-              Stockholm University and KTH Royal Institute of Technology.
+              A 9-year collaboration between 40+ companies and public organisations, together with researchers from
+              Stockholm University and KTH/Royal Institute of Technology.
             </p>
           </section>
           <section id="cartoon">
@@ -37,14 +36,14 @@ export default function Home(props) {
           </section>
           <section id="our-method">
             <h5>
-              Our method starts by scouting the most committed decision makers and domain experts in your organisation.
-              Together we define a business challenge, such as implementing a new business strategy or creating new
-              digitised products or services. Then we start to solve the challenge by iterations by rapid prototyping
-              and weekly dialogues.
+              The T3 method begins by scouting the most committed decision makers and domain experts in your
+              organisation. Together we define a business challenge, such as implementing a new business strategy or
+              creating new digitised products or services. Then we start to solve the challenge through iterations via
+              rapid prototyping and weekly dialogues.
               <br />
               <br />
-              What makes us different is our scientifically designed and smart integration tools which guarantees
-              sustainable and scalable solutions.
+              What makes us different is our scientifically-designed integration tools which guarantee sustainable and
+              scalable solutions.
             </h5>
           </section>
           <section id="our-clients" className="space-y-12">
@@ -55,13 +54,15 @@ export default function Home(props) {
         <footer id="contact">
           <h2>Let's solve a problem!</h2>
           <address className="not-italic text-xl text-right px-4">
-            Jens Olhsson<br />
+            Jens Olhsson
+            <br />
             <span className="text-gray-600">CEO at {name}</span>
             <br />
             <br />
             <a href="mailto:jens.olhsson@r3t.info">jens.olhsson@r3t.info</a>
             <br />
-            <a href="tel:+46706445732">070-644 57 32</a><br />
+            <a href="tel:+46706445732">070-644 57 32</a>
+            <br />
             <a href="https://www.linkedin.com/company/realtreat-ab/" target="_blank" rel="noopener noreferrer">
               {name} LinkedIn
             </a>
