59ef22720434b103d329794e5bb98684a204a6f2
bbkfed <balthazarberling+fed@gmail.com>
2020-10-21 15:27:05 +0200

    worked on responsivity, all screens, almost done

--- components/client.js
+++ components/client.js
@@ -1,18 +1,18 @@
 export default function Client({ clientData }) {
   return (
-    <div key={clientData.id} id={`client-${clientData.id}`} className="border-8 border-teal-300 rounded-xl p-4 flex flex-col">
+    <div key={clientData.id} id={`client-${clientData.id}`} className="border-8 border-teal-300 rounded-xl sm:rounded-2xl p-4 md:px-6 md:py-4 lg:px-4 flex flex-col lg:w-1/3 lg:mx-6 xl:mx-8 lg:self-start lg:mb-6">
       <div id={`client-header-${clientData.id}`} className="w-full flex flex-row flex-nowrap pb-4">
         <div className="flex-shrink-0">
-          <img src={clientData.img} alt={clientData.name} className="h-auto w-20 rounded-3xl" />
+          <img src={clientData.img} alt={clientData.name} className="h-auto w-20 sm:w-24 xl:w-32 rounded-3xl" />
         </div>
-        <div className="pl-2 flex-shrink">
+        <div className="pl-3 flex-shrink">
           <h4>
-            {clientData.name} <p className="text-lg text-gray-600 font-normal tracking-tight leading-tight">{clientData.position} <span className="text-gray-900">at</span> {clientData.company}</p>
+            {clientData.name} <p className="text-lg md:text-md text-gray-600 font-normal tracking-tight md:tracking-normal leading-tight">{clientData.position} <span className="text-gray-900">at</span> {clientData.company}</p>
           </h4>
         </div>
       </div>
       <div id={`client-body-${clientData.id}`} className="w-full">
-        <p className="leading-snug mb-0">{clientData.content}</p>
+        <p className="leading-snug sm:leading-normal lg:text-lg mb-0">{clientData.content}</p>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -15,39 +15,48 @@ export default function Home(props) {
         <title>{name}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container p-2 space-y-8">
-        <header className="py-2 px-1 bg-teal-300 rounded-md">
+      <div className="container mx-auto p-2 lg:py-4 space-y-8 lg:space-y-20">
+        <header className="py-2 xl:py-4 px-1 bg-teal-300 rounded-md sm:px-16 md:px-20 lg:px-40 xl:px-56">
           <h1 className="text-6xl text-gray-900 font-bold tracking-tighter">{name}</h1>
         </header>
 
-        <main className="space-y-10">
-          <section id="who-we-are">
-            <p className="text-2xl font-medium tracking-tight">
+        <main className="space-y-10 lg:space-y-20">
+          <section id="who-we-are" className="sm:px-16 md:px-20 lg:px-48 xl:px-56">
+            <p className="text-2xl font-medium tracking-tight lg:tracking-normal lg:leading-normal">
               We are a digital services company changing the way digital strategies, management, and consulting are
               delivered. We work with the 3T method which is based on Swedish research.*
             </p>
-            <p className="pl-0">
+            <p className="pl-0 md:w-3/4 md:ml-auto xl:w-3/5 xl:text-lg">
               *A 9-year collaboration between 40+ companies and public organisations, together with researchers from
               Stockholm University and KTH/Royal Institute of Technology.
             </p>
           </section>
-          <section id="cartoon">
-            <img src="images/the-executive.png" alt="The Executive, [name of the author]" className="w-full h-auto" />
+          <section id="3t-border">
+            <div className="h-4 sm:h-6 bg-teal-300 rounded-md flex justify-around sm:justify-evenly">
+              <div className="bg-white w-8 md:w-6 h-full"></div>
+              <div className="bg-white w-8 md:w-6 h-full"></div>
+              <div className="bg-white w-8 md:w-6 h-full"></div>
+            </div>
           </section>
-          <section id="our-method">
+          <section id="our-method" className="sm:px-16 md:px-20 lg:px-48 xl:px-56">
             <p>
               <span className="font-medium">The 3T method</span> begins by scouting the most committed decision makers
               and domain experts in your organisation.
             </p>
             <p>
-            Together we define opportunities such as improving your business strategies or creating new digitised products or services.
+              Together we define opportunities such as improving your business strategies or creating new digitised
+              products or services.
             </p>
             <p>Then we implement these opportunities through iterations via rapid prototyping with weekly dialogues.</p>
             <p>
-            What makes us different is our scientifically-designed integration tools which guarantee sustainable and scalable solutions.
+              What makes us different is our scientifically-designed integration tools which guarantee sustainable and
+              scalable solutions.
             </p>
           </section>
-          <section id="our-clients" className="space-y-5">
+          <section
+            id="our-clients"
+            className="space-y-5 sm:space-y-8 lg:space-y-0 sm:px-32 md:px-48 lg:px-0 lg:flex lg:flex-row lg:flex-wrap lg:justify-center"
+          >
             {allClientsAsCards}
           </section>
         </main>
@@ -56,23 +65,29 @@ export default function Home(props) {
           <div className="mb-4 h-20">
             <img src="images/arrow-down.svg" alt="arrow down" className="h-full mx-auto" />
           </div>
-          <h2 className="text-center text-3xl font-medium mb-6">Let’s realise your opportunities!</h2>
-          <address className="my-4 not-italic">
-            <h4>
-              Jens Ohlsson
-              <p className="text-lg text-gray-600 font-normal tracking-tight leading-tight">
-                Chief Executive Officer <span className="text-gray-900">at</span> {name}
-              </p>
-            </h4>
-            <a href="mailto:jens.ohlsson@r3t.info">jens.ohlsson@r3t.info</a><br />
-            <a href="tel:+46706445732">070-644 57 32</a><br /><br />
-            <a href="https://www.linkedin.com/company/realtreat-ab/" target="_blank" rel="noopener noreferrer">
-              {name} LinkedIn
-            </a>
-          </address>
-          <div className="my-4">
-            <img src="images/jens-ohlsson.jpg" alt="Jens Ohlsson" className="rounded-xl" />
+          <h2 className="text-center text-3xl xl:text-4xl font-medium mb-6 lg:mb-20 sm:px-32">Let’s realise your opportunities!</h2>
+          <div id="address-and-portrait" className="lg:flex lg:flex-wrap lg:px-20 lg:justify-center">
+            <address className="my-4 md:mt-12 lg:mt-20 lg:pl-4 not-italic sm:px-16 lg:px-0 sm:text-center lg:text-left md:text-xl md:leading-relaxed lg:w-1/2 lg:self-start lg:order-2">
+              <h4 className="md:text-2xl">
+                Jens Ohlsson
+                <p className="text-lg md:text-xl text-gray-600 font-normal tracking-tight leading-tight">
+                  Chief Executive Officer <span className="text-gray-900">at</span> {name}
+                </p>
+              </h4>
+              <a href="mailto:jens.ohlsson@r3t.info">jens.ohlsson@r3t.info</a>
+              <br />
+              <a href="tel:+46706445732">070-644 57 32</a>
+              <br />
+              <br />
+              <a href="https://www.linkedin.com/company/realtreat-ab/" target="_blank" rel="noopener noreferrer">
+                {name} LinkedIn
+              </a>
+            </address>
+            <div className="mt-4 sm:mt-8 mb-2 lg:mb-10 sm:px-32 md:px-40 lg:px-0 lg:pr-4 xl:pl-24 lg:w-1/2 lg:order-1">
+              <img src="images/jens-ohlsson.jpg" alt="Jens Ohlsson" className="rounded-xl sm:rounded-2xl lg:mx-auto" />
+            </div>
           </div>
+          <div className="h-4 sm:h-6 bg-teal-300 rounded-md sm:rounded-lg sm:mx-32 md:mx-40 lg:mx-0"></div>
         </footer>
       </div>
     </>
